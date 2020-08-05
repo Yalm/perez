74172fb2c4b1910a8b8aadaed050af11eddcb144
Yalm <mcondori@dtransforma.com>
2020-08-04 22:38:37 -0500

    add page

---
+++ docs/precache-manifest.82e6dd38226a181f193ff75fded27383.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "441c661f69cc4ebcd088",
+    "url": "/css/app.41f42f0f.css"
+  },
+  {
+    "revision": "de82ba71957d786eaa0a",
+    "url": "/css/chunk-20a9831f.9dfcb765.css"
+  },
+  {
+    "revision": "7c650c50bd888cc40dce",
+    "url": "/css/chunk-2bf98904.5826431b.css"
+  },
+  {
+    "revision": "e5af21383d0f3dbb4e0c",
+    "url": "/css/chunk-589e3ce0.85384154.css"
+  },
+  {
+    "revision": "7c170a8d9e4ed6829291",
+    "url": "/css/chunk-6b9d86bd.143234cc.css"
+  },
+  {
+    "revision": "8a9c9f0d2216de57ed42",
+    "url": "/css/chunk-ee08d168.de882c18.css"
+  },
+  {
+    "revision": "7e55035d88ab70e32e57718124da7237",
+    "url": "/img/logo.7e55035d.svg"
+  },
+  {
+    "revision": "7d008476016947ab19adba7962349b9e",
+    "url": "/img/whatsapp.7d008476.svg"
+  },
+  {
+    "revision": "6eadc95343382cde32d6480a68a20a3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "441c661f69cc4ebcd088",
+    "url": "/js/app-legacy.0510089b.js"
+  },
+  {
+    "revision": "de82ba71957d786eaa0a",
+    "url": "/js/chunk-20a9831f-legacy.99fe3c80.js"
+  },
+  {
+    "revision": "7c650c50bd888cc40dce",
+    "url": "/js/chunk-2bf98904-legacy.451fbb3f.js"
+  },
+  {
+    "revision": "a548bb301b0a78a4ed67",
+    "url": "/js/chunk-2d0ae5a4-legacy.4fc0d5c0.js"
+  },
+  {
+    "revision": "e5af21383d0f3dbb4e0c",
+    "url": "/js/chunk-589e3ce0-legacy.08454d40.js"
+  },
+  {
+    "revision": "7c170a8d9e4ed6829291",
+    "url": "/js/chunk-6b9d86bd-legacy.a550de22.js"
+  },
+  {
+    "revision": "8a9c9f0d2216de57ed42",
+    "url": "/js/chunk-ee08d168-legacy.5b7b946a.js"
+  },
+  {
+    "revision": "209765a299eb3cdc59e8",
+    "url": "/js/chunk-vendors-legacy.41863851.js"
+  },
+  {
+    "revision": "10fc81f46bc9a49b04b0853d29bcb30e",
+    "url": "/manifest.json"
+  }
+]);
