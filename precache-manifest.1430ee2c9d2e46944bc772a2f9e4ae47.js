61110e80701283dc6b6e168c5244d92fb32bcef2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-05 04:55:56 +0000

    Deploy Yalm/perez to github.com/Yalm/perez.git:gh-pages

---
+++ precache-manifest.1430ee2c9d2e46944bc772a2f9e4ae47.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1d22302566a36bd1c8",
+    "url": "/perez/css/app.527f875a.css"
+  },
+  {
+    "revision": "82cc76f94e46538efa27",
+    "url": "/perez/css/chunk-16621b0a.1fce9a13.css"
+  },
+  {
+    "revision": "30445e0321a588fc0cca",
+    "url": "/perez/css/chunk-2cc3ac3c.3f8c79c3.css"
+  },
+  {
+    "revision": "efd89ebdb46d798749b6",
+    "url": "/perez/css/chunk-6ec1b819.143234cc.css"
+  },
+  {
+    "revision": "20314ea03b478d9ea716",
+    "url": "/perez/css/chunk-a428e9d0.ca5ba74e.css"
+  },
+  {
+    "revision": "570aebe1fa5bba5e06c3",
+    "url": "/perez/css/chunk-a51a2244.94efa780.css"
+  },
+  {
+    "revision": "e10a6798590725f85cb187da8d4832bd",
+    "url": "/perez/img/logo.e10a6798.svg"
+  },
+  {
+    "revision": "cb59a783bae935b11235644c9f530b84",
+    "url": "/perez/img/whatsapp.cb59a783.svg"
+  },
+  {
+    "revision": "bfac8fc9b12516e64e0f5aaf3c9e86ad",
+    "url": "/perez/index.html"
+  },
+  {
+    "revision": "9a1d22302566a36bd1c8",
+    "url": "/perez/js/app.bd849b71.js"
+  },
+  {
+    "revision": "82cc76f94e46538efa27",
+    "url": "/perez/js/chunk-16621b0a.fbd3bd67.js"
+  },
+  {
+    "revision": "30445e0321a588fc0cca",
+    "url": "/perez/js/chunk-2cc3ac3c.12bbac3f.js"
+  },
+  {
+    "revision": "5d1c2c8985b99418976e",
+    "url": "/perez/js/chunk-2d0ae5a4.fa9d554b.js"
+  },
+  {
+    "revision": "efd89ebdb46d798749b6",
+    "url": "/perez/js/chunk-6ec1b819.6adf4a6e.js"
+  },
+  {
+    "revision": "20314ea03b478d9ea716",
+    "url": "/perez/js/chunk-a428e9d0.2c3b89d0.js"
+  },
+  {
+    "revision": "570aebe1fa5bba5e06c3",
+    "url": "/perez/js/chunk-a51a2244.075debb7.js"
+  },
+  {
+    "revision": "e0ffc135f0ddc0e90b49",
+    "url": "/perez/js/chunk-vendors.5de8bc0e.js"
+  },
+  {
+    "revision": "10fc81f46bc9a49b04b0853d29bcb30e",
+    "url": "/perez/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/perez/robots.txt"
+  }
+]);
---
+++ precache-manifest.1e7874dff2298aaecc50f8fc61180943.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eea4575e3ecb50f2c0d",
+    "url": "/perez/css/app.527f875a.css"
+  },
+  {
+    "revision": "788dbd60ec49c8a66eec",
+    "url": "/perez/css/chunk-16621b0a.1fce9a13.css"
+  },
+  {
+    "revision": "1e63960fc0ee54285c4b",
+    "url": "/perez/css/chunk-2cc3ac3c.3f8c79c3.css"
+  },
+  {
+    "revision": "7c170a8d9e4ed6829291",
+    "url": "/perez/css/chunk-6b9d86bd.143234cc.css"
+  },
+  {
+    "revision": "96884ea55dfb535dcb9e",
+    "url": "/perez/css/chunk-a428e9d0.ca5ba74e.css"
+  },
+  {
+    "revision": "cbacfdf2e062506ac686",
+    "url": "/perez/css/chunk-a51a2244.94efa780.css"
+  },
+  {
+    "revision": "e10a6798590725f85cb187da8d4832bd",
+    "url": "/perez/img/logo.e10a6798.svg"
+  },
+  {
+    "revision": "cb59a783bae935b11235644c9f530b84",
+    "url": "/perez/img/whatsapp.cb59a783.svg"
+  },
+  {
+    "revision": "73eb2b04f98dbd17215ed4b7f98cb879",
+    "url": "/perez/index.html"
+  },
+  {
+    "revision": "8eea4575e3ecb50f2c0d",
+    "url": "/perez/js/app-legacy.e427b105.js"
+  },
+  {
+    "revision": "788dbd60ec49c8a66eec",
+    "url": "/perez/js/chunk-16621b0a-legacy.56f0210b.js"
+  },
+  {
+    "revision": "1e63960fc0ee54285c4b",
+    "url": "/perez/js/chunk-2cc3ac3c-legacy.2f358c53.js"
+  },
+  {
+    "revision": "5d1c2c8985b99418976e",
+    "url": "/perez/js/chunk-2d0ae5a4-legacy.fa9d554b.js"
+  },
+  {
+    "revision": "7c170a8d9e4ed6829291",
+    "url": "/perez/js/chunk-6b9d86bd-legacy.a550de22.js"
+  },
+  {
+    "revision": "96884ea55dfb535dcb9e",
+    "url": "/perez/js/chunk-a428e9d0-legacy.792af6ca.js"
+  },
+  {
+    "revision": "cbacfdf2e062506ac686",
+    "url": "/perez/js/chunk-a51a2244-legacy.0cc7435c.js"
+  },
+  {
+    "revision": "209765a299eb3cdc59e8",
+    "url": "/perez/js/chunk-vendors-legacy.41863851.js"
+  },
+  {
+    "revision": "10fc81f46bc9a49b04b0853d29bcb30e",
+    "url": "/perez/manifest.json"
+  }
+]);
